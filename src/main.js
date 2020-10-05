b824384362f40ba2091addb8d695d298abbcf9b9
lmgibson <landon.g42@gmail.com>
2020-10-04 22:16:05 -0700

    Testing if airtable API works

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
-let trueDoggoLoverCheck = [
-    'Are you sure?',
-    'Do you really want to see photos of doggos?',
-    'They\'re pretty cute huh',
-    'Wow you really like doggo\'s',
-    'Okay, I won\'t bother you anymore',
-    'Just one more. Okay, now enjoy!',
-]
-
-let i=0
-
 // $ ( document ).ready(function() {
 //     $( "a" ).hover(function(event)  {
 //         $( "a" ).toggleClass( "test" )
