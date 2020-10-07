6ac78b41df6e228bd800abe4bb7b5a9ceee3211d
lmgibson <landon.g42@gmail.com>
2020-10-06 21:48:43 -0700

    Removing unneccessary files. Javascript basically not required.

--- src/main.js
+++ src/main.js
@@ -1,41 +1,6 @@
 'use strict'
 
-let doggoMotivations = [
-    'A good boy',
-    'A good girl',
-    'Woof!',
-    'Chonk',
-    'Hi human!'
-]
-
-
-// Adding doggo-image generation
-let dogButton = document.getElementById('give-dog')
-
-dogButton.addEventListener("click", evt => {
-    
-    let dogDiv = document.getElementById('dog-pic')
-    let num = Math.floor(Math.random() * doggoMotivations.length)
-    console.log(num);
-
-    fetch('https://api.thedogapi.com/v1/images/search')
-        .then(res => res.json())
-        .then(dogs => {
-            dogs.forEach(dog => {
-                dogDiv.innerHTML = `<h3>${doggoMotivations[num]}</h3>
-                <img src="${dog.url}" alt="woofers" />`
-            });
-        })
-})
-
-// Adding the date
-let today = new Date()
-let formatDate = today.toDateString()
-let dateText = document.getElementById('date')
-
-dateText.innerHTML = `Today's date: ${formatDate}\n`
-
-// Checking knoweldge of 
+// Checking knoweldge
 let homeButton = document.getElementById('home-button')
 
 homeButton.addEventListener('click', evt => {
